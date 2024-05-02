b3638ed2765c1dcb8e051d4e871c467cfdeaa6aa
rahulio96 <rahgupta96@gmail.com>
2024-05-01 20:28:09 -0700

    Blank page with scaffold

---
+++ lib/pages/home.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+
+// use stl to auto-generate
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+    );
+  }
+}
